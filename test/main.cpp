0bd3a51505486baa1b2da5025e1afa0520a3d364
ReturnZer0 <raofei23@gmail.com>
2018-08-17 23:49:43 +0800

    parse testcase

--- test/main.cpp
+++ test/main.cpp
@@ -1,14 +1,34 @@
 #include <string>
 #include <fstream>
 #include <iostream>
-#include <stdlib.h>
 
 #include "../lib/uritemplate.hpp"
 #include "../ext/json/include/nlohmann/json.hpp"
 
-
 using json = nlohmann::json;
 using std::string;
+
+struct LevelTestCase{
+	uint level;
+	std::map<std::string,std::string> variables;
+	std::vector< std::pair<string,string> > testcases;
+};
+
+bool parseTestCase(const json& parseJson, LevelTestCase& result){
+	result.level = parseJson["level"];
+	
+	json Jvariables = parseJson["variables"];
+	for(json::iterator it = Jvariables.begin(); it != Jvariables.end(); ++it)
+	{
+		result.variables.insert(it.key(),it.value().get<string>());
+	}
+
+	json JtestCases = parseJson["testcases"];
+	for(json::iterator it = Jvariables.begin(); it != Jvariables.end(); ++it)
+	{
+		result.testcases.push_back(std::pair<string,string>(it[0].get<string>(),it[1].get<string>()));
+	}
+};
 
 int main(int argc, char* argv[]){
 	for (int i = 0; i<argc; i++)
@@ -26,9 +46,13 @@ int main(int argc, char* argv[]){
 	}
 	catch(const std::exception& e)
 	{
-		std::cerr << e.what() << '\n';
+		std::cerr << "json parse error ! the error is " <<e.what() << '\n';
+		exit(1);
 	}
 
-	system("pause");
+	json Level1Test = j["Level 1 Examples"];
+	LevelTestCase level1Case;
+	parseTestCase(Level1Test,level1Case);
+	
     return 0;
 }
