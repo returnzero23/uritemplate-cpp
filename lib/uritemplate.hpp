308a22d8927dad5121375f4ac0625eac5911d8b0
ReturnZer0 <raofei23@gmail.com>
2018-08-18 09:07:05 +0800

    add uritemplate importance interface

--- lib/uritemplate.hpp
+++ lib/uritemplate.hpp
@@ -31,10 +31,22 @@ enum Operator{
 
 struct TemplateComponent{
     std::string literal;
-    std::vector< std::pair< Operator, std::vector<VarSpec> > > varList;
+    std::pair< Operator, std::vector<VarSpec> >  varList;
+public:
+	TemplateComponent(const std::string inLiteral){
+		literal = inLiteral;
+	};
+
+	TemplateComponent(const std::pair< Operator, std::vector<VarSpec> >  inVarList){
+		varList = inVarList;
+	};
 };
 
 class TemplateVar{
+public:
+	TemplateVar(const std::string varBuf){
+
+	};
 private:
     std::string scalar;
     std::vector<std::string> list;
@@ -43,11 +55,48 @@ private:
 
 class UriTemplate{
 public:
-	UriTemplate(std::string Template){
+	UriTemplate(std::string src_template){
+		using string = std::string;
+		bool in_varlist = false;
+		string buf;
+		for(string::iterator ch = src_template.begin(); ch != src_template.end(); ch++)
+		{
+			if(in_varlist && *ch == '}'){
+				components.push_back(parse_varlist(buf));
+				buf.clear();
+				in_varlist = false;
+				continue;
+			}
+
+			if(in_varlist && *ch == '{'){
+				if( buf.size() > 0 ){
+					components.push_back(TemplateComponent(buf));
+					buf.clear();
+				}
+				components.push_back(parse_varlist(buf));
+				buf.clear();
+				in_varlist = false;
+				continue;
+			}
+		}
+
+		if( buf.size() > 0 ){
+			components.push_back(TemplateComponent(buf));
+		}
 	};
 
-	std::string expand(std::map<std::string,std::string> vars){
+	std::string expand(std::map<std::string,std::string> inVars){
+		//vars = inVars;
 		return std::string();
+	};
+
+	std::string expand(std::string key, std::string val){
+		vars.insert(std::pair<std::string,TemplateVar>(key,TemplateVar(val)));
+		return std::string();
+	}
+private:
+	TemplateComponent parse_varlist(std::string){
+		return TemplateComponent(std::string());
 	}
 private:
     std::vector<TemplateComponent> components;
