396550f379d52ac80222c69c726c63b6768eef4a
ReturnZer0 <raofei23@gmail.com>
2018-08-26 15:47:07 +0800

    modify testcase

--- test/main.cpp
+++ test/main.cpp
@@ -48,7 +48,7 @@ public:
 	std::map<std::string,VarType> variables;
 	std::map<std::string,std::string> variablesString;
 
-	std::vector< std::pair<string,string> > testcases;
+	std::vector< std::pair<std::string,std::vector<std::string> > > testcases;
 };
 
 bool parseTestCase(const json& parseJson, LevelTestCase& result){
@@ -60,25 +60,37 @@ bool parseTestCase(const json& parseJson, LevelTestCase& result){
 		json Jvariables = parseJson["variables"];
 		for(json::iterator it = Jvariables.begin(); it != Jvariables.end(); ++it)
 		{
-			// if(it.value().is_array()){
-			// 	//todo
-			// }else if(it.value().is_object()){
-			// 	//todo
-			// }else{
-			// 	result.variables.insert(std::pair<string,LevelTestCase::VarType>(it.key(),LevelTestCase::VarType(it.value().get<string>())));
-			// }
-			result.variables.insert(std::pair<string,LevelTestCase::VarType>(it.key(),LevelTestCase::VarType(it.value().dump())));
+			if(it.value().is_array()){
+				std::vector<std::string> listStr;
+				for(json::iterator it2 = it->begin(); it2 != it->end(); ++it2)
+				{
+					listStr.push_back(it2->get<std::string>());
+				}
+				result.setVectorVariables(it.key(),listStr);
+			}else if(it.value().is_object()){
+				std::map<std::string,std::string> mapStr;
+				for(json::iterator it2 = it->begin(); it2 != it->end(); ++it2)
+				{
+					mapStr.insert(std::pair<std::string,std::string>(it2.key(),it2.value().get<std::string>()));
+				}
+				result.setMapVariables(it.key(),mapStr);
+			}else{
+				result.variables.insert(std::pair<string,LevelTestCase::VarType>(it.key(),LevelTestCase::VarType(it.value().get<string>())));
+			}
 		}
 
 		json JtestCases = parseJson["testcases"];
 		for(json::iterator it = JtestCases.begin(); it != JtestCases.end(); ++it)
 		{
+			std::vector<std::string> resultList;
 			if(it->at(1).is_array()){
-				//std::cout << "array" << std::endl;
-				//todo
+				for(json::iterator it2 = it->at(1).begin(); it2 != it->at(1).end(); ++it2){
+					resultList.push_back(it2->get<std::string>());
+				}
 			}else{
-				result.testcases.push_back(std::make_pair<string,string>(it->at(0).get<string>(),it->at(1).get<string>()));
+				resultList.push_back(it->at(1).get<std::string>());
 			}
+			result.testcases.push_back(std::pair<std::string,std::vector<std::string> >(it->at(0).get<string>(),resultList));
 		}
 		return true;
 	}
@@ -90,8 +102,6 @@ bool parseTestCase(const json& parseJson, LevelTestCase& result){
 };
 
 int main(int argc, char* argv[]){
-	// for (int i = 0; i<argc; i++)
-    //     std::cout << argv[i] << std::endl;
 
 	string filePath = argv[1];
 
@@ -101,7 +111,6 @@ int main(int argc, char* argv[]){
 	try
 	{
 		i >> j;
-		//std::cout << j << std::endl;
 	}
 	catch(const std::exception& e)
 	{
@@ -109,48 +118,56 @@ int main(int argc, char* argv[]){
 		exit(1);
 	}
 
-	json Level1Test = j["Level 2 Examples"];
-	LevelTestCase level1Case;
-	if(parseTestCase(Level1Test,level1Case)){
-		for(std::pair<string,string> element : level1Case.testcases){
-			UriTemplate uri(std::get<0>(element));
-			std::map<std::string,std::string> varMap;
-			for(auto& ele : level1Case.variables){
-				varMap.insert(std::pair<std::string,std::string>(ele.first,ele.second.scalarVar.substr(1,ele.second.scalarVar.size()-2)));
-			}
-			if(std::get<1>(element) == uri.expand(varMap)){
-				std::cout << "pass" ;
-			}else{
-				std::cout << "failed" ;
-			}
-			std::cout << std::endl;
+	for(int i = 1; i < 5; i++){
+		char str[10];
+		sprintf(str,"%d",i);
+		std::string LevelStr = "Level " + string(str) + " Examples";
+		std::cout << "test " << LevelStr << std::endl;
+		json Level1Test = j[LevelStr];
+		LevelTestCase level1Case;
+		if(parseTestCase(Level1Test,level1Case)){
+			for(std::pair<string,std::vector<string> > element : level1Case.testcases){
+				UriTemplate uri(std::get<0>(element));
+				std::map<std::string,std::string> varMap;
+				for(auto& ele : level1Case.variables){
+					
+					switch (ele.second.type)
+					{
+						case LevelTestCase::VarType::Type::Scalar:
+							uri.set(ele.first,ele.second.scalarVar);
+							break;
+						case LevelTestCase::VarType::Type::Map:
+							uri.set(ele.first,ele.second.mapVars);
+							break;
+						case LevelTestCase::VarType::Type::Vec:
+							uri.set(ele.first,ele.second.vecVars);
+							break;
+					
+						default:
+							break;
+					}
+				}
 
-			// for(auto& ele : level1Case.variables){
-			// 	if(ele.second.type == LevelTestCase::VarType::Vec){
-			// 		if(std::get<1>(element) == uri.expand(ele.first, ele.second.vecVars)){
-			// 			std::cout << "pass" ;
-			// 		}else{
-			// 			std::cout << "failed" ;
-			// 		}
-			// 		std::cout << std::endl;
-			// 	}else if (ele.second.type == LevelTestCase::VarType::Map){
-			// 		if(std::get<1>(element) == uri.expand(ele.second.mapVars)){
-			// 			std::cout << "pass" ;
-			// 		}else{
-			// 			std::cout << "failed" ;
-			// 		}
-			// 		std::cout << std::endl;
-			// 	}else{
- 			// 		if(std::get<1>(element) == uri.expand(ele.first, ele.second.scalarVar.substr(1,ele.second.scalarVar.size()-2))){
-			// 			std::cout << "pass" ;
-			// 		}else{
-			// 			std::cout << "failed" ;
-			// 		}
-			// 		std::cout << std::endl;
-			// 	}
-			// }
+				bool pass = false;
+				std::string result = uri.build();
+				for(auto& ele :std::get<1>(element))
+				{
+					if(ele == result){
+						pass = true;
+					}
+				}
+				
+				if(pass){
+					std::cout << "pass" ;
+				}else{
+					std::cout << "failed" ;
+				}
+				std::cout << std::endl;
+			}
 		}
 	}
+
+	
 
 
 
